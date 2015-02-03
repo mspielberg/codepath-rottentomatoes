7be44fb737ee2675a3f60f7c30dfcfee584c902d
Miles Spielberg <miles@box.com>
2015-02-02 20:13:53 -0800

    Initial Commit

---
+++ RottenTomatoes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RottenTomatoes
+//
+//  Created by Miles Spielberg on 2/2/15.
+//  Copyright (c) 2015 OrionNet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RottenTomatoes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RottenTomatoes
+//
+//  Created by Miles Spielberg on 2/2/15.
+//  Copyright (c) 2015 OrionNet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
